1cfdf3182c8c931d90e84208c254d7ee176d94b7
Muhammad Haroon Rasheed <haroonrasheed211f@gmail.com>
2024-07-17 22:48:38 +0500

    add lazy loading in about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,20 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { NotFoundComponent } from './compnonents/not-found/not-found.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./components/about/about.component').then(
+        (mod) => mod.AboutComponent
+      ),
+  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  // wildcard component 404 page
+  { path: '**', component: NotFoundComponent },
 ];
