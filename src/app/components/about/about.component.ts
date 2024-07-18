8053d863770beadcb1e67acad5f7dc4c0156fd4d
Muhammad Haroon Rasheed <haroonrasheed211f@gmail.com>
2024-07-18 18:39:38 +0500

    add about page ui

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,12 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      defaultFOCWorker: [''],
+      defaultCEUWorker: [''],
+      pickupCode: [''],
+      chargeCode: [''],
+      warrantTypeEntryCode: [''],
+      leinSupportIND: [''],
+      scbwAutoSCH: [''],
+      scbwAutoSCHType: [''],
+      scbwAutoSCHOut: [''],
+      scbwAutoSCHMinWeeks: [''],
+      defaultParentingTimeBondAMT: [''],
+      splitEventDefaultSpacing: [''],
+      defaultAppearanceBondAMT: [''],
+      defaultAppearanceBondPercent: [''],
+      courtOriginatingAgency: [''],
+      defaultLaboratoryID: [''],
+      benchWarrantRemarks: [''],
+      imageDocumentsFTPId: [''],
+      imageDocumentsFTPServer: [''],
+      password: [''],
+      path: [''],
+      iwnText: [''],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
